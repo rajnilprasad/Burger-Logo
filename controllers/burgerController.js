25878fc99e0c9420d61e3a1d89a366179655c0ee
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-07-10 15:47:33 -0700

    initial commit

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,57 +1,35 @@
-// dependencies
-const express = require("express");
-// import the model to use its db functions for burger.js
-const burger = require("../models/burger.js");
-
-// create the router for the app, and export 
-const router = express.Router();
-// Create routes and set up logic where required.
-router.get("/", (req, res) => { 
-    res.redirect("/burgers")
-    });
-router.get("/burgers"), (req, res) => {
-    burger.selectAll(function(data){
-        res.render('index', {
-            data: data
-        })
-    })
-}
-
-//add new Burger to the db
-router.post("/api/burgers", (req, res) => {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
-        // send back the ID of the new burger
-        res.json({ id: result.insertId });
-    });
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
 });
-
-// set burger devoured status to true
-router.put("/api/burgers/:id", (req, res) => {
-    const condition = "id =  " + req.params.id;
-    console.log("condition", condition);
-    burger.updateOne({ devoured: req.body.devoured }, condition, (result) => {
-        if (result.changedRows === 0) {
-            // if no rows were changed, then the ID must not exit, so 404
-            return res.status(404).end();
-        }
-        else {
-            res.status(200).end();
-        }
-    });
+router.get("/burgers", function(req, res) {
+  // express callback response by calling burger.selectAllBurger
+  burger.all(function(burgerData) {
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+    res.render("index", { burger_data: burgerData });
+  });
 });
-
-// delete Burger
-router.delete("/api/burgers/:id", (req, res) => {
-    const condition = "id = " + req.params.id;
-    console.log("condition", condition);
-
-    burger.deleteOne(condition, (result) => {
-        if (result.changedRows === 0) {
-            // if no rows were changed, then the ID must not exit, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
+  });
+});
+// put route -> back to index
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .then in Ajax
+    res.sendStatus(200);
+  });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-// Dependencies
-const express = require(`express`);
+var express = require("express");
 
-const PORT = process.env.PORT || 3030;
-
-const app = express();
-
+var PORT = process.env.PORT || 8080;
+var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(`public`));
-
-// Parse application body as JSON
+app.use(express.static("public"));
+// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Set Handlebars
-const exphbs = require(`express-handlebars`);
-
-app.engine(`handlebars`, exphbs({ defaultLayout: `main` }));
-app.set(`view engine`, `handlebars`);
-
-// Import routes and give the server access to them.
-var routes = require(`./controllers/burgerController`);
-
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+var routes = require("./controllers/burgerController.js");
 app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log(`App now listening at localhost: ${PORT}`);
+app.listen(PORT, function () {
+  console.log("Listening on port:%s", PORT);
 });
